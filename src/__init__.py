7b4c06a1fa029da5eb1e932084fea47df108ee44
Makoto KUNO <mkt-kuno@iis.u-tokyo.ac.jp>
2025-12-04 23:04:27 +0900

    Revert "Merge pull request #12 from mkt-kuno/copilot/add-taichi-ggui-visualization"
    This reverts commit 3c38e46a7b411e713605385e7010b2f1ad01cc38, reversing
    changes made to 25101406f8892046d3df297d4a5afeca0f30c61d.

--- src/__init__.py
+++ src/__init__.py
@@ -36,11 +36,6 @@ from .rtm import (
     init_taichi,
     reset_taichi,
 )
-from .visualization import (
-    RTMViewer,
-    create_realtime_callback,
-    save_image_numpy,
-)
 
 __version__ = '2.0.0'
 __author__ = 'Yutaro Hara'
@@ -57,7 +52,4 @@ __all__ = [
     'prepare_image_data',
     'create_visualization_data',
     'save_stacked_results',
-    'RTMViewer',
-    'create_realtime_callback',
-    'save_image_numpy',
 ]
